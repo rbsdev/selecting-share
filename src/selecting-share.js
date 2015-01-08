9ce984782288786684c92f5aae5b4b416205769d
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-08 10:21:39 -0200

    improved structure

---
+++ src/selecting-share.js
@@ -0,0 +1,16 @@
+{
+  "name": "selecting-share",
+  "version": "0.0.0",
+  "authors": [
+    "evandrolg"
+  ],
+  "description": "",
+  "main": "",
+  "keywords": [],
+  "ignore": [
+    "**/.*",
+    "test",
+    "*.md"
+  ],
+  "license": "MIT"
+}
