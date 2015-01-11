db37571aaaee29eeb33dfd7b8f0551563617b779
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-11 18:39:39 -0200

    created instance test

---
+++ test/spec.js
@@ -0,0 +1,9 @@
+describe('selectingShare', function() {
+
+  describe('instance', function() {
+    it('should exists selectingShare object', function() {
+      expect(window.selectingShare).be.an('function');
+    });
+  });
+
+});
